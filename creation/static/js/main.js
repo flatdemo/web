b55f79556d67c5cf3716d05e5672c426691c94e9
sunmy <sunmengyuan@wanmeizhensuo.com>
2016-07-21 16:14:40 +0800

    toggle menu

--- creation/static/js/main.js
+++ creation/static/js/main.js
@@ -9,12 +9,20 @@ var main = {
     do: function () {
         var _this = this;
         _this.parallaxBar();
+        _this.toggleMenu();
     },
     parallaxBar: function () {
         var $bar = $('.js_bar');
         $win.on('scroll', function () {
             var top = $body.scrollTop();
             $bar.css('background-position-y', -top);
+        });
+    },
+    toggleMenu: function () {
+        var $btn = $('.js_toggle_menu');
+        var $menu = $('.js_menu');
+        $btn.on('click', function () {
+            $menu.slideToggle();
         });
     }
 };
